c2da47277abda9bdaa992c001f35b5f088119967
Volodymyr <volodymyrquo@gmail.com>
2020-12-18 09:45:27 +0200

    added typography

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  helloThereStyle: {
+    fontStyle: 'oblique',
+    color: 'red',
+    fontSize: '30px',
+  },
+  button: {
+    color: 'brown',
+  },
+});
 
 const App = () => {
+  const classes = useStyles();
   return (
     <div>
-      <Button>This is our first button</Button>
+      <Typography
+        color='primary'
+        variant='h1'
+        className={classes.helloThereStyle}>
+        Hey there
+      </Typography>
+      <Button variant='outlined' color='secondary' className={classes.button}>
+        This is our first button
+      </Button>
     </div>
   );
 };
