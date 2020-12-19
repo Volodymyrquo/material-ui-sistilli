e02565d78707bf22d91c1e3466bf2e1d78dfb8bd
Volodymyr <volodymyrquo@gmail.com>
2020-12-19 07:11:00 +0200

    added grid to app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import { Button, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  helloThereStyle: {
-    fontStyle: 'oblique',
-    color: 'red',
-    fontSize: '30px',
-  },
-  button: {
-    color: 'brown',
-  },
-});
 
 const App = () => {
-  const classes = useStyles();
   return (
-    <div>
-      <Typography
-        color='primary'
-        variant='h1'
-        className={classes.helloThereStyle}>
-        Hey there
-      </Typography>
-      <Button variant='outlined' color='secondary' className={classes.button}>
-        This is our first button
-      </Button>
-    </div>
+    <Grid contaiter direction='column'>
+      <Grid item>This is will be Header</Grid>
+      <Grid item>This is will be Content</Grid>
+    </Grid>
   );
 };
 
